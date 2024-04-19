76fb3dda9caed99faaa777142b7668eb348ce539
Don De Dieu Bundjoko <99693799+MonsieurBdk@users.noreply.github.com>
2024-04-19 22:23:35 +0100

    donwload song logic

--- lib/data/repositories/song_repository.dart
+++ lib/data/repositories/song_repository.dart
@@ -1,4 +1,6 @@
 import 'package:Gael/data/api/client/dio_client.dart';
+import 'package:Gael/data/data_base/database_client.dart';
+import 'package:Gael/data/models/song_model.dart';
 import 'package:Gael/utils/config/app_config.dart';
 import 'package:dio/dio.dart';
 import 'package:shared_preferences/shared_preferences.dart';
@@ -14,6 +16,26 @@ class SongRepository {
   Future<Response> getSongs({int? page}) async {
     final response = await dioClient.get(AppConfig.songsUrl, queryParameters: {"page": page});
     return response;
+  }
+
+  Future<List<Song>> getSongFromDb() async{
+    List<Song> songs = [];
+    var db = DatabaseHelper.instance;
+    await db.fetchSongs().then(
+            (value){
+          songs = value;
+        }
+    );
+    return songs;
+  }
+  upsertSong({required Song song})async{
+    var db = DatabaseHelper.instance;
+    db.upsertSong(song);
+  }
+
+  deleteSong({required String songId}){
+    var db = DatabaseHelper.instance;
+    db.deleteSong(songId);
   }
 
 
--- lib/data/repositories/splash_repository.dart
+++ lib/data/repositories/splash_repository.dart
@@ -61,6 +61,4 @@ class SplashRepository{
     return database;
   }
 
-  
-
 }
